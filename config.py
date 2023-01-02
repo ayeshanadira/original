809c205f3f01ca99c72ac295b6bd6be228f7c20f
ayeshanadira <109727112+ayeshanadira@users.noreply.github.com>
2023-01-02 19:22:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.cil4uwyoii6v.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "ayeshanadirabintiirwan-bucket"
 customregion = "us-east-1"
